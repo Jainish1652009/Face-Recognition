331f81b5e7ebccbc506953e5cc3b8b49e5591d7d
Jainish1652009 <88092366+Jainish1652009@users.noreply.github.com>
2021-09-29 17:32:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,4 +18,24 @@ Webcam.set({
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/xPEySUu0m/model.json',modeLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/l6W2Pct6h/model.json',modeLoaded);
+
+function modeLoaded(){
+    console.log('Model Loaded!');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML =results[0].confidence.toFixed(3);
+    }
+}
